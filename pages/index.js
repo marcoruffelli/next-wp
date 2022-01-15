a8f2fc978671d0a1db85029970ebc6e61474cbfe
Giancarlo Ruffelli <giancarlo.ruffelli@gmail.com>
2022-01-15 16:49:53 +0100

    modifica giancarlo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
-export default function Home({posts}) {
+export default function Home({ posts }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,37 +12,31 @@ export default function Home({posts}) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          MODIFICA senza ricompilazione su visualstudiocode
-        </h1>
-        <img src="https://d2j5yhogz3pnm1.cloudfront.net/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova"/>
-        <img src="https://imagesnextwp.s3.eu-central-1.amazonaws.com/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova"/>
+        <h1 className={styles.title}>questa nmodifica è stata effettuata da giancarlo</h1>
+        <img src="https://d2j5yhogz3pnm1.cloudfront.net/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
+        <img src="https://imagesnextwp.s3.eu-central-1.amazonaws.com/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
         {posts.map((post) => {
           return (
             <div className={styles.card} key={post.id}>
               <h2>{post.title.rendered}</h2>
             </div>
-          )
+          );
         })}
       </main>
 
-
-
-      <footer className={styles.footer}>
-        
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-  const response = await fetch('https://adminwp.pepenet.info/wp-json/wp/v2/posts')
+export async function getStaticProps() {
+  const response = await fetch("https://adminwp.pepenet.info/wp-json/wp/v2/posts");
   //const response = await fetch('https://jsonplaceholder.typicode.com/users')
-  const data = await response.json()
+  const data = await response.json();
 
-  return{
-    props:{
+  return {
+    props: {
       posts: data,
     },
-  }
+  };
 }
