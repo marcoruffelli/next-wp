52d3ff0717218a5bc3d159bcb4ad6036d5bbd579
Marco Ruffelli <marco.ruffelli@gmail.com>
2022-01-16 12:25:57 +0100

    elimina aws apmlify

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 import '../styles/globals.css'
 
-import Amplify, { Storage } from 'aws-amplify';
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
