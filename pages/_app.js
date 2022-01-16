137adbd042939cab86dccfda27a15dfd7be050fc
Marco Ruffelli <marco.ruffelli@gmail.com>
2022-01-16 11:59:13 +0100

    aggiunge storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
