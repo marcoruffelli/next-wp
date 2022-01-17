c66ecfe0ee916531ae177a1adb439a635fd70124
Marco Ruffelli <marco.ruffelli@gmail.com>
2022-01-17 20:35:49 +0100

    modifica serale

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({posts}) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          modifica per verifica cloudfront
+          modifica serale
         </h1>
         <img src="https://d2j5yhogz3pnm1.cloudfront.net/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova"/>
         <img src="https://imagesnextwp.s3.eu-central-1.amazonaws.com/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova"/>
