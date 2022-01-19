3409178ef7b6994ef29633db58fbce9b2cd62d63
Giancarlo Ruffelli <giancarlo.ruffelli@gmail.com>
2022-01-19 11:09:52 +0100

    modifica del 19

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>modifica dell 15:43</h1>
+        <h1 className={styles.title}>modifica del 19</h1>
         <img src="https://d2j5yhogz3pnm1.cloudfront.net/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
         <img src="https://imagesnextwp.s3.eu-central-1.amazonaws.com/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
         <img src="https://pepenet.info/download.jpg" alt="prova" />
