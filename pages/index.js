60c32e68c46a9b7167b5908c9209080997102555
Giancarlo Ruffelli <giancarlo.ruffelli@gmail.com>
2022-02-06 23:33:47 +0100

    foto da bucket s3

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home({ posts }) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>pomeriggio del 19</h1>
-        <img src="https://d2j5yhogz3pnm1.cloudfront.net/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
-        <img src="https://imagesnextwp.s3.eu-central-1.amazonaws.com/wp-content/uploads/2022/01/11214427/Cattura.png" alt="prova" />
-        <img src="https://pepenet.info/download.jpg" alt="prova" />
+        <img src="https://pepenet.info/img/foto-1.webp" alt="prova" />
+        <img src="https://pepenet.info/img/foto-2.webp" alt="prova" />
+        <img src="https://pepenet.info/img/foto-3.webp" alt="prova" />
         {posts.map((post) => {
           return (
             <div className={styles.card} key={post.id}>
